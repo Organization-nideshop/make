c1c0b74e0d2f92664e709b39a22bc3765b51460c
fable1207 <815721052@qq.com>
2018-07-30 17:09:26 +0800

    日期选择器aaaaaaaaaaaaa

--- make/pages/ucenter/addBirthday/addBirthday.js
+++ make/pages/ucenter/addBirthday/addBirthday.js
@@ -5,7 +5,7 @@ const years = []
 const months = []
 const days = []
 
-for (let i = 1990; i <= date.getFullYear(); i++) {
+for (let i = 1900; i <= date.getFullYear(); i++) {
     years.push(i)
 }
 
@@ -28,7 +28,7 @@ Page({
         showDate:'',
         value: [9999, 1, 1],
         showTips:'block',
-        showPicker:'none'
+        // showPicker:'none'
     },
     goToPage:function (e) {
         var url = e.currentTarget.dataset.id;
@@ -36,14 +36,14 @@ Page({
             url:url
         })
     },
-    bindDateChange:function (e) {
+    /*bindDateChange:function (e) {
         console.log('picker发送选择改变，携带值为', e.detail.value);
         console.log(this.data.myDate);
         this.setData({
             date: e.detail.value,
             showTips:'none'
         })
-    },
+    },*/
     onLoad:function () {
         console.log(this.data.myDate+"biaoji");
     },
@@ -56,12 +56,60 @@ Page({
             day: this.data.days[val[2]],*/
             showDate: this.data.years[val[0]]+'-'+this.data.months[val[1]]+'-'+this.data.days[val[2]],
             showTips:'none',
-            showPicker:'none'
+            // showPicker:'none'
         })
     },
-    showPicker:function () {
+    /*showPicker:function () {
         this.setData({
             showPicker:'block'
         })
-    }
+    }*/
+    showPicker:function () {
+        this.showModal();
+    },
+    showModal:function () {
+        console.log("1111");
+        var animation = wx.createAnimation({
+            duration:100,
+            timingFunction:"linear",
+            delay:0
+        })
+        this.animation = animation;
+        animation.translateY(300).step();
+        this.setData({
+            animationData:animation.export(),
+            modalShow:true
+        })
+        setTimeout(function () {
+            animation.translateY(0).step();
+            this.setData({
+                animationData:animation.export(),
+                modalShow:true
+            })
+        }.bind(this,100))
+    },
+    modalHide:function () {
+        this.hideModal();
+    },
+    hideModal:function () {
+        console.log("2222");
+        var animation = wx.createAnimation({
+            duration:100,
+            timingFunction:"linear",
+            delay:0
+        })
+        this.animation = animation;
+        animation.translateY(300).step();
+        this.setData({
+            animationData:animation.export(),
+            modalShow:false
+        })
+        setTimeout(function () {
+            animation.translateY(0).step();
+            this.setData({
+                animationData:animation.export(),
+                modalShow:false
+            })
+        }.bind(this,100))
+    },
 })
--- make/pages/ucenter/completeInfo/completeInfo.js
+++ make/pages/ucenter/completeInfo/completeInfo.js
@@ -1,14 +1,89 @@
 var getDate= new Date();
+
+const date = new Date()
+const years = []
+const months = []
+const days = []
+
+for (let i = 1900; i <= date.getFullYear(); i++) {
+    years.push(i)
+}
+
+for (let i = 1 ; i <= 12; i++) {
+    months.push(i)
+}
+
+for (let i = 1 ; i <= 31; i++) {
+    days.push(i)
+}
 Page({
     data:{
-        date:'1988-6-22',
-        myDate:getDate.getFullYear() + '-' + (getDate.getMonth() + 1) + '-' + getDate.getDate(),
+        years: years,
+        months: months,
+        days: days,
+        showDate:'1990-1-1',
+        value: [9999, 1, 1],
+        // showTips:'block',
+        // date:'1988-6-22',
+        // myDate:getDate.getFullYear() + '-' + (getDate.getMonth() + 1) + '-' + getDate.getDate(),
     },
-    bindDateChange:function (e) {
-        console.log('picker发送选择改变，携带值为', e.detail.value);
-        console.log(this.data.myDate);
+    bindChange: function(e) {
+        const val = e.detail.value;
         this.setData({
-            date: e.detail.value,
+            /*year: this.data.years[val[0]],
+            month: this.data.months[val[1]],
+            day: this.data.days[val[2]],*/
+            showDate: this.data.years[val[0]]+'-'+this.data.months[val[1]]+'-'+this.data.days[val[2]],
+            // showTips:'none',
+            // showPicker:'none'
         })
+    },
+    showPicker:function () {
+        this.showModal();
+    },
+    showModal:function () {
+        console.log("1111");
+        var animation = wx.createAnimation({
+            duration:100,
+            timingFunction:"linear",
+            delay:0
+        })
+        this.animation = animation;
+        animation.translateY(300).step();
+        this.setData({
+            animationData:animation.export(),
+            modalShow:true
+        })
+        setTimeout(function () {
+            animation.translateY(0).step();
+            this.setData({
+                animationData:animation.export(),
+                modalShow:true
+            })
+        }.bind(this,100))
+    },
+    modalHide:function () {
+        this.hideModal();
+    },
+    hideModal:function () {
+        console.log("2222");
+        var animation = wx.createAnimation({
+            duration:100,
+            timingFunction:"linear",
+            delay:0
+        })
+        this.animation = animation;
+        animation.translateY(300).step();
+        this.setData({
+            animationData:animation.export(),
+            modalShow:false
+        })
+        setTimeout(function () {
+            animation.translateY(0).step();
+            this.setData({
+                animationData:animation.export(),
+                modalShow:false
+            })
+        }.bind(this,100))
     },
 })
