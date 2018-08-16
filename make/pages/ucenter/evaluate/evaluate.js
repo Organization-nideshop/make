f7995bb7e920d023ce502adb23567f7cd6f95a30
fable1207 <815721052@qq.com>
2018-08-16 17:53:14 +0800

    服务评价

---
+++ make/pages/ucenter/evaluate/evaluate.js
@@ -0,0 +1,70 @@
+// pages/ucenter/evaluate/evaluate.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+      upLoadImg:[
+          {id:0,url:'../../../static/img/good-item3.jpg'},
+          {id:2,url:'../../../static/img/good-item2.jpg'},
+          {id:3,url:'../../../static/img/good-item1.jpg'}
+      ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
